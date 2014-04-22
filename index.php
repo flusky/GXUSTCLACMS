e465ba428f62dd40fd25ef07a49f16d7643cb07a
Bluice Zhen <bluice.zhen@gmail.com>
2014-04-22 02:59:50 -0400

    V1.0 仅仅修改了入口文件

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-define('APP_DEBUG', true);		//开启调试模式，项目部署时请注释该行
+define('APP_DEBUG', true);								//开启调试模式，项目部署时请注释该行
+define('ROOT',		dirname(__FILE__));					//设置根目录的绝对路径
+define('DOMAIN',	'http://127.0.0.1:1000/gxustcla/');	//设置域名
+define('APP_NAME',	'Application');						//应用名
+define('APP_PATH',	'./Application/' );					//应用目录
+define('COOKIE_EXPIRE',24*3600);						//Cookie有效期
 
-define('ROOT',		dirname(__FILE__));
-define('DOMAIN',	'http://127.0.0.1:1000/gxustcla/');
-define('APP_NAME',	'Application');
-define('APP_PATH',	'./Application/' );
-//cookie保存一天
-define('COOKIE_EXPIRE',24*3600);
-
-require('./ThinkPHP/ThinkPHP.php');
+require('./ThinkPHP/ThinkPHP.php');						//加载框架文件
